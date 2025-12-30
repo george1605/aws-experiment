7bcf4e3ba816ab3cf3058477dbeede43e3542066
george1605 <george160505@gmail.com>
2025-12-30 15:51:59 +0200

    Added EC2 Client Support (Tested Starting/Stopping/Describing Instaces cmds)

--- mycode.js
+++ mycode.js
@@ -1,4 +1,6 @@
 const { S3Client, PutObjectCommand, ListObjectsV2Command, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { EC2Client, EC2, DescribeInstancesCommand, StartInstancesCommand, StopInstancesCommand } = require('@aws-sdk/client-ec2');
+
 const fs = require('fs');
 require('dotenv').config();
 
@@ -9,8 +11,31 @@ const client = new S3Client({ region: "eu-north-1",
     }
 });
 
+const ec2Client = new EC2Client({ region: "eu-north-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
+
+async function startInstance()
+{
+    await ec2Client.send(new StartInstancesCommand({
+        InstanceIds: ['i-0cfb79ec48c519938']
+    }))
+}
+
+async function stopInstance()
+{
+    await ec2Client.send(new StopInstancesCommand({
+        InstanceIds: ['i-0cfb79ec48c519938']
+    }))
+}
+
 async function main() {
   const bucket = "s3-bckt-1605"; // name of the bucket
+
+  startInstance();
 
   // Upload
   await client.send(new PutObjectCommand({
@@ -25,6 +50,12 @@ async function main() {
   if (data.Contents) {
     data.Contents.forEach(obj => console.log(obj.Key, obj.Size));
   }
+
+  console.log('EC2 Instances');
+  const instances = await ec2Client.send(new DescribeInstancesCommand({}));
+  console.log(instances.Reservations[0].Instances);
+
+  // stopInstance(); 
 
   // const body = await client.send(new GetObjectCommand({ Bucket: bucket }));
 }
